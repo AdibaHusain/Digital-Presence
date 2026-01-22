2d1fc5c44fcdb86a535c318c50ac243adfd4c991
Adiba <adibaknp@gmail.com>
2026-01-23 01:43:40 +0530

    Added the constants.js data

---
+++ src/utils/constants.js
@@ -0,0 +1,37 @@
+export const PERSONAL_INFO = {
+  name: "Adiba Husain",
+  title: "B.Tech CSE (AI) Student",
+  email: "adibaknp@gmail.com",
+  location: "Kanpur, India",
+  tagline: "Passionate Web Developer | AI Enthusiast | Problem Solver",
+  resume: "/Adiba_Husain_resume(6).pdf",
+  bio: [
+  "Computer Science undergraduate specializing in Artificial Intelligence, with a strong interest in building impactful digital products.",
+  "Passionate about web development, problem-solving, and transforming ideas into clean, user-focused experiences.",
+  "Constantly learning, experimenting, and pushing myself through real-world projects and modern technologies."
+],
+};
+
+export const SOCIAL_LINKS = {
+  github: "https://github.com/AdibaHusain",
+  linkedin: "https://www.linkedin.com/in/adiba-husain-6b0856278/",
+  twitter: "https://x.com/adiba_husain786",
+};
+
+export const STATS = [
+  { label: "Projects Completed", value: "3" },
+  { label: "Technologies", value: "10+" },
+];
+
+export const ABOUT_STATS = [
+  { label: "Leetcode Questions", value: "350+" },
+  { label: "Coding Streaks", value: "100+ days" },
+  { label: "GitHub Repositories", value: "8+" },
+];
+
+export const NAV_LINKS = [
+  { id: "about", label: "About" },
+  { id: "skills", label: "Skills" },
+  { id: "projects", label: "Projects" },
+  { id: "contact", label: "Contact" },
+];
